1d791405bde29648a14fd7f47094d0aaa7097245
moru <e1028092@student.tuwien.ac.at>
2016-11-24 20:43:59 +0100

    Commented out build of example file. Buildable via build.sh now.

--- build-prep.sh
+++ build-prep.sh
@@ -16,15 +16,15 @@ pdflatex $CLASS.dtx
 pdflatex $CLASS.ins
 
 # Build the vutinfth example document
-pdflatex $SOURCE
-bibtex   $SOURCE
-pdflatex $SOURCE
-pdflatex $SOURCE
-makeindex -t $SOURCE.glg -s $SOURCE.ist -o $SOURCE.gls $SOURCE.glo
-makeindex -t $SOURCE.alg -s $SOURCE.ist -o $SOURCE.acr $SOURCE.acn
-makeindex -t $SOURCE.ilg -o $SOURCE.ind $SOURCE.idx
-pdflatex $SOURCE
-pdflatex $SOURCE
+# pdflatex $SOURCE
+# bibtex   $SOURCE
+# pdflatex $SOURCE
+# pdflatex $SOURCE
+# makeindex -t $SOURCE.glg -s $SOURCE.ist -o $SOURCE.gls $SOURCE.glo
+# makeindex -t $SOURCE.alg -s $SOURCE.ist -o $SOURCE.acr $SOURCE.acn
+# makeindex -t $SOURCE.ilg -o $SOURCE.ind $SOURCE.idx
+# pdflatex $SOURCE
+# pdflatex $SOURCE
 
 echo
 echo
