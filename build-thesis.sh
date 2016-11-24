a3c721477da5e0dcd9e3ee3a92efe9aa4e690afa
moru <e1028092@student.tuwien.ac.at>
2016-11-24 20:06:34 +0100

    Added watch-mode to build.

--- build-thesis.sh
+++ build-thesis.sh
@@ -1,21 +1,57 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright (C) 2014-2016 by Thomas Auzinger <thomas@auzinger.name>
 
 # Replace the 'x' in the next line with the name of the thesis' main LaTeX document without the '.tex' extension
-SOURCE=x
+#SOURCE="$1"
 
+for arg in "$@"
+do
+    echo "got arg: $arg"
+
+    case $arg in
+    -w|--watch)
+    watch=true
+    ;;
+    *)
+    source="$arg"
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+function buildLatex {
+  echo "\n\n\n------ (RE)BUILDING pdflatex -------\n\n\n"
+  pdflatex -interaction=nonstopmode $source
+}
 # Build the thesis document
-pdflatex $SOURCE
-bibtex   $SOURCE
-pdflatex $SOURCE
-pdflatex $SOURCE
-makeindex -t $SOURCE.glg -s $SOURCE.ist -o $SOURCE.gls $SOURCE.glo
-makeindex -t $SOURCE.alg -s $SOURCE.ist -o $SOURCE.acr $SOURCE.acn
-makeindex -t $SOURCE.ilg -o $SOURCE.ind $SOURCE.idx
-pdflatex $SOURCE
-pdflatex $SOURCE
+#buildLatex="echo '\n\n\n------ (RE)BUILDING non-bibtex/non-index -------\n\n\n' && pdflatex -interaction=nonstopmode $source"
+buildLatex
 
-echo
-echo
-echo Thesis document compiled.
+echo "\n\n\n------ (RE)BUILDING bibtex -------\n\n\n"
+bibtex   $source
+
+buildLatex
+buildLatex
+
+echo "\n\n\n------ (RE)BUILDING indices -------\n\n\n"
+makeindex -t $source.glg -s $source.ist -o $source.gls $source.glo
+makeindex -t $source.alg -s $source.ist -o $source.acr $source.acn
+makeindex -t $source.ilg -o $source.ind $source.idx
+
+buildLatex
+#$buildLatex
+
+if [[ "$watch" = true ]]; then
+echo "\n\n\n------ STARTING WATCH -------\n\n\n"
+  while inotifywait -e close_write *.tex;
+  do
+    # build every time a tex file changes
+    buildLatex
+  done
+else
+  # build one last time
+  buildLatex
+  echo "Thesis document compiled."
+fi
 sleep
