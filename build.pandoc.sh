648872831a28fb32ddc161ffa504143bd323c4fa
moru <e1028092@student.tuwien.ac.at>
2018-04-28 20:51:02 +0200

    Newlines in build-script.

--- build.pandoc.sh
+++ build.pandoc.sh
@@ -11,4 +11,14 @@
 
 # pandoc 00_frontmatter.md 01_abstract.md 02_problemdescription.md -o export.tex && pandoc export.tex -o export.pdf
 # cat 00_frontmatter.md 01_abstract.md 02_problemdescription.md | pandoc -t latex | pandoc -f latex -o export.pdf
-pandoc 00_frontmatter.md 01_abstract.md 02_problemdescription.md 02a_state_of_the_art.md --pdf-engine=xelatex --template ./templates/eisvogel-book.latex --number-sections --listings --toc -o export.pdf
+pandoc \
+00_frontmatter.md \
+01_abstract.md \
+02_problemdescription.md \
+02a_state_of_the_art.md \
+--pdf-engine=xelatex \
+--template ./templates/eisvogel-book.latex \
+--number-sections \
+--listings \
+--toc \
+-o export.pdf
