5b5fc079b66710448ecce5ea79ba7126ef991b02
moru <e1028092@student.tuwien.ac.at>
2018-04-28 14:03:34 +0200

    Eisvogel theme.

--- 00_frontmatter.md
+++ 00_frontmatter.md
@@ -3,10 +3,11 @@ title: '<Some Title>'
 author: Kevin Singer
 tags: [some, nice, tags]
 date: 25.01.2018
+titlepage: true
 
-documentclass: report
+# documentclass: report
 #documentclass: article
-# documentclass: book
+documentclass: scrbook
 
 # bibliography: references.json
 # link-citations: true
--- build.pandoc.sh
+++ build.pandoc.sh
@@ -1,3 +1,6 @@
+# eisvogel theme: https://github.com/Wandmalfarbe/pandoc-latex-template
+# run sudo apt install texlive-fonts-extra first
+
 # pandoc 00_frontmatter.md 01_abstract.md 02_problemdescription.md -o export.tex && pandoc export.tex -o export.pdf
 # cat 00_frontmatter.md 01_abstract.md 02_problemdescription.md | pandoc -t latex | pandoc -f latex -o export.pdf
-pandoc 00_frontmatter.md 01_abstract.md 02_problemdescription.md 02a_state_of_the_art.md --pdf-engine=xelatex -o export.pdf
+pandoc 00_frontmatter.md 01_abstract.md 02_problemdescription.md 02a_state_of_the_art.md --pdf-engine=xelatex --template eisvogel --number-sections --listings -o export.pdf
