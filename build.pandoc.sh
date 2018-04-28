6de233f1f7acc6a356d93bb58eaba64258656e07
moru <e1028092@student.tuwien.ac.at>
2018-04-28 15:00:13 +0200

    Documented some of the libs i had to install.

--- build.pandoc.sh
+++ build.pandoc.sh
@@ -1,5 +1,10 @@
 # eisvogel theme: https://github.com/Wandmalfarbe/pandoc-latex-template
 # run sudo apt install texlive-fonts-extra first
+# needed installs: 
+# texlive
+# texlive-fonts-extra (for the eisvogel theme)
+# xetex(?) or pdflatex/texlive
+# librsvg2-2, librsvg2-bin (when including svg/pdf-images) + inkscape(?)
 
 # pandoc 00_frontmatter.md 01_abstract.md 02_problemdescription.md -o export.tex && pandoc export.tex -o export.pdf
 # cat 00_frontmatter.md 01_abstract.md 02_problemdescription.md | pandoc -t latex | pandoc -f latex -o export.pdf
