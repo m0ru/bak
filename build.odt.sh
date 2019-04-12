9dfd82fe9750dc5a946ea1b5609d40afc420a58b
moru <e1028092@student.tuwien.ac.at>
2019-04-12 18:16:15 +0200

    Quick'n'dirty script to build odt.

---
+++ build.odt.sh
@@ -0,0 +1,21 @@
+
+mkdir -p generated
+
+pandoc \
+00_frontmatter.md \
+01_abstract.md \
+02_problemdescription.md \
+02a_state_of_the_art.md \
+03_methods.md \
+03_01_hevner.md \
+04_solution.md \
+06_summary_and_future_work.md \
+99_references_header.md \
+--csl=./templates/chicago-17-author-date.csl \
+--filter ./templates/pandoc-crossref \
+--filter pandoc-citeproc \
+--number-sections \
+--listings \
+--toc \
+-s \
+-o generated/export.odt
